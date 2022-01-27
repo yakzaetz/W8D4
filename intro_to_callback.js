16c9f5bcfe05fa3a5d907363803d60d8b265d55f
Qien Gao <qiengao@Qiens-MBP.lan>
2022-01-27 17:35:54 -0500

    finish my bind

--- intro_to_callback.js
+++ intro_to_callback.js
@@ -1,91 +1,118 @@
-// class Clock {
-//     constructor(){
-//         this.date = new Date();
-//         this.hh = this.date.getHours();
-//         this.mm = this.date.getMinutes();
-//         this.ss = this.date.getSeconds();
-//         // this.hh = 23;
-//         // this.mm = 59;
-//         // this.ss = 50;
+class Clock {
+    constructor(){
+        this.date = new Date();
+        this.hh = this.date.getHours();
+        this.mm = this.date.getMinutes();
+        this.ss = this.date.getSeconds();
+        // this.hh = 23;
+        // this.mm = 59;
+        // this.ss = 50;
         
-//         // printTime()
-//         // setTimeout(() => {
-//         //     console.log(printTime())
-//         // }, 1000);
-//     }    
+        // printTime()
+        // setTimeout(() => {
+        //     console.log(printTime())
+        // }, 1000);
+    }    
 
-//     printTime() {
-//         console.log(`The time is ${this.hh}:${this.mm}:${this.ss}`)
-//     }
+    printTime() {
+        console.log(`The time is ${this.hh}:${this.mm}:${this.ss}`)
+    }
 
-//     _tick() {  
-//         // setInterval(() => {
-//         //     debugger
-//         //     this.printTime()
-//         // }, 1000);
-//         let that = this;
-//         setInterval(function(){
-//             //parseInt
-//             if (parseInt(that.ss) < 59) {
-//                 that.ss++;
-//                 that.ss = that.ss < 10 ? "0" + that.ss : that.ss
-//             } else {
-//                 that.ss = '00';
-//                 that.mm++;
-//                 that.mm = that.mm < 10 ? "0" + that.mm : that.mm
-//                 if (parseInt(that.mm) >= 59) {
-//                     that.mm = '00'
-//                     that.hh++;
-//                     that.hh = that.hh < 10 ? "0" + that.hh : that.hh
-//                     if (parseInt(that.hh) >= 23) {
-//                         that.hh = '00'
-//                     }   
-//                 }
-//             }
-//             // if (parseInt(that.mm) < 59){
-//             //     that.mm++;
-//             // } else {
-//             //     that.mm = '00';
-//             //     that.hh++;
-//             // }
-//             that.printTime();
-//         }, 1000)
-//     }
+    _tick() {  
+        // setInterval(() => {
+        //     debugger
+        //     this.printTime()
+        // }, 1000);
+        let that = this;
+        setInterval(function(){
+            //parseInt
+            if (parseInt(that.ss) < 59) {
+                that.ss++;
+                that.ss = that.ss < 10 ? "0" + that.ss : that.ss
+            } else {
+                that.ss = '00';
+                that.mm++;
+                that.mm = that.mm < 10 ? "0" + that.mm : that.mm
+                if (parseInt(that.mm) >= 59) {
+                    that.mm = '00'
+                    that.hh++;
+                    that.hh = that.hh < 10 ? "0" + that.hh : that.hh
+                    if (parseInt(that.hh) >= 23) {
+                        that.hh = '00'
+                    }   
+                }
+            }
+            // if (parseInt(that.mm) < 59){
+            //     that.mm++;
+            // } else {
+            //     that.mm = '00';
+            //     that.hh++;
+            // }
+            that.printTime();
+        }, 1000)
+    }
 
-//     // _methodName indicates it a private method
-// }
-// const clock = new Clock();
-// clock._tick();
-
-// const readline = require('readline');
-
-// const reader = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// function addNumbers(sum, numsLeft, completionCallback){
-//     if (numsLeft === 0) {
-//         console.log("hello")
-//         completionCallback(sum);
-//         return
-//     }
-//     if (numsLeft > 0){
-//         console.log("hi")
-//         reader.question("Enter a number", function(input){
-//             sum += (parseInt(input))
-//             // console.log(sum += parseInt(input))
-//             addNumbers(sum, (numsLeft - 1), completionCallback)
-//         })
-//     }
-//     // numsLeft -= 1
-//      // 3 => numsleft = numsleft - 1
-// }
-
-// addNumbers(0, 3, sum => {
-//     console.log(`Total Sum: ${sum}`)
-//     reader.close()
-// });
+    // _methodName indicates it a private method
+}
+const clock = new Clock();
+clock._tick();
 
 
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback){
+    if (numsLeft === 0) {
+        console.log("hello")
+        completionCallback(sum);
+        return
+    }
+    if (numsLeft > 0){
+        console.log("hi")
+        reader.question("Enter a number", function(input){
+            sum += (parseInt(input))
+            // console.log(sum += parseInt(input))
+            addNumbers(sum, (numsLeft - 1), completionCallback)
+        })
+    }
+    // numsLeft -= 1
+     // 3 => numsleft = numsleft - 1
+}
+
+addNumbers(0, 3, sum => {
+    console.log(`Total Sum: ${sum}`)
+    reader.close()
+});
+
+
+Function.prototype.myBind = function(context) {
+    let that = this;
+    return resultFunction = function() {
+        return that.apply(context);
+    }
+}
+
+class Lamp {
+    constructor() {
+        this.name = "a lamp";
+    }
+}
+
+const turnOn = function () {
+    console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
 
