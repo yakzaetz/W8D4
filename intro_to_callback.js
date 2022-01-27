47f7730e785f6a9a30ff1ea269c157007e536f1d
Qien Gao <qiengao@Qiens-MBP.lan>
2022-01-27 15:45:16 -0500

    we have a clock now

--- intro_to_callback.js
+++ intro_to_callback.js
@@ -4,15 +4,56 @@ class Clock {
         this.hh = this.date.getHours();
         this.mm = this.date.getMinutes();
         this.ss = this.date.getSeconds();
-
+        // this.hh = 23;
+        // this.mm = 59;
+        // this.ss = 50;
+        
         // printTime()
+        // setTimeout(() => {
+        //     console.log(printTime())
+        // }, 1000);
+    }    
 
-        setTimeout(() => {
-            console.log(printTime())
-        }, 1000);
+    printTime() {
+        console.log(`The time is ${this.hh}:${this.mm}:${this.ss}`)
     }
 
-    
-}
+    _tick() {  
+        // setInterval(() => {
+        //     debugger
+        //     this.printTime()
+        // }, 1000);
+        let that = this;
+        setInterval(function(){
+            //parseInt
+            if (parseInt(that.ss) < 59) {
+                that.ss++;
+                that.ss = that.ss < 10 ? "0" + that.ss : that.ss
+            } else {
+                that.ss = '00';
+                that.mm++;
+                that.mm = that.mm < 10 ? "0" + that.mm : that.mm
+                if (parseInt(that.mm) >= 59) {
+                    that.mm = '00'
+                    that.hh++;
+                    that.hh = that.hh < 10 ? "0" + that.hh : that.hh
+                    if (parseInt(that.hh) >= 23) {
+                        that.hh = '00'
+                    }   
+                }
+            }
+            // if (parseInt(that.mm) < 59){
+            //     that.mm++;
+            // } else {
+            //     that.mm = '00';
+            //     that.hh++;
+            // }
+            that.printTime();
+        }, 1000)
+    }
 
+    // _methodName indicates it a private method
+}
 const clock = new Clock();
+clock._tick();
+
